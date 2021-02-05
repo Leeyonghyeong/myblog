280953083c9603df8d1493f909d4e2575f20eb8c
Leeyonghyeong <tont1990@gmail.com>
2021-02-05 15:58:46 +0900

    first commit

---
+++ content/__about/resume-en.md
@@ -0,0 +1,13 @@
+---
+title: 'about'
+date: 2019-1-27 16:21:13
+lang: 'en'
+---
+
+# Your name
+
+<div align="center">
+
+_안녕하세요 저의 첫 블로그 입니다._
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `L.devLog`,
+  description: `Blog posted about ...`,
+  author: `L.dev`,
+  introduction: `다시 꾸는 개발의 꿈`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
